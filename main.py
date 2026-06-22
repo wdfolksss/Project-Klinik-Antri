20de8aa192ef606efe9d72e9afbdcb72c7ccc954
wdfolksss <amaliawarda411@gmail.com>
2026-06-22 14:35:55 +0700

    buat sistem antrean

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ def init_db():
 def home():
     return render_template("pasien/registrasi.html")
     
+  #REGISRASI PASIEN  
 @app.route("/registrasi", methods=["GET", "POST"])
 def registrasi():
     if request.method == "POST":
@@ -81,8 +82,75 @@ def registrasi():
         """, (nik, nama, umur, jenis_kelamin, no_hp))
         conn.commit()
         conn.close()
-
         return redirect("/")
+    
+    #ambil antrean
+@app.route("/ambil-antrean", methods=["GET","POST"])
+def ambil_antrean():
+    if request.method == "POST":
+        poli = request.form["poli"]
+        kode_poli = {
+            "Poli Umum":"PU",
+            "Poli Gigi":"PG",
+            "Poli Anak":"PA"
+        }
+
+        kode = kode_poli[poli]
+        conn = get_db()
+
+            # ambil nomor terakhir berdasarkan poli
+        terakhir = conn.execute("""
+            SELECT no_antrean 
+            FROM antrean
+            WHERE poli = ?
+            ORDER BY id DESC
+            LIMIT 1
+        """, (poli,)).fetchone()
+
+        if terakhir:
+            nomor = int(terakhir["no_antrean"][2:]) + 1
+        else:
+            nomor = 1
+        no_antrean = f"{kode}{nomor:03d}"
+
+        # ambil pasien terakhir yang daftar
+        pasien = conn.execute("""
+            SELECT id FROM pasien
+            ORDER BY id DESC
+            LIMIT 1
+        """).fetchone()
+
+        conn.execute("""
+            INSERT INTO antrean
+            (id_pasien, no_antrean, poli, status, waktu_daftar)
+
+            VALUES (?, ?, ?, ?, datetime('now'))
+        """,
+        (
+            pasien["id"],
+            no_antrean,
+            poli,
+            "MENUNGGU"
+        ))
+
+        conn.commit()
+        conn.close()
+
+        return render_template(
+            "pasien/status_antrean.html",
+            nomor=no_antrean,
+            poli=poli
+        )
+
+    return render_template("pasien/ambil_antrean.html")
+
+# @app.route("/data")
+# def data():
+#     conn = get_db()
+#     pasien = conn.execute("SELECT * FROM pasien").fetchall()
+#     conn.close()
+
+#     return str([dict(row) for row in pasien])
 
 if __name__ == "__main__":
     init_db()
